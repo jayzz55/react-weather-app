b46f7195fd44b40bbdd3a407008a8c9daaba9015
Jaya Wijono <jaya.wijono@realestate.com.au>
2017-01-12 00:12:59 +1100

    add home container

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import GetLocation from './GetLocation'
+import GetLocationContainer from '../containers/GetLocationContainer'
 import { homeStyles } from '../styles/index'
 
 function Home () {
   return (
     <div style={homeStyles.container}>
       <h1 style={homeStyles.header}>Enter a City and State</h1>
-      <GetLocation />
+      <GetLocationContainer />
     </div>
   );
 }
---
+++ app/containers/GetLocationContainer.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import GetLocation from '../components/GetLocation'
+
+const GetLocationContainer = React.createClass({
+  getDefaultProps () {
+    return {
+      direction: 'column'
+    }
+  },
+  propTypes: {
+    direction: PropTypes.string
+  },
+  getInitialState () {
+    return {
+      city: ''
+    }
+  },
+  handleUpdateCity (e) {
+    this.setState({
+      city: e.target.value
+    })
+  },
+  handleSubmitCity () {
+    console.log(this.state.city);
+  },
+  render () {
+    return (
+      <GetLocation
+        direction={this.props.city}
+        onUpdateCity={this.handleUpdateCity}
+        onSubmitCity={this.handleSubmitCity}
+        city={this.state.city} />
+    )
+  }
+});
+
+export default GetLocationContainer
